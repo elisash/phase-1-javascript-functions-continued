4dced571ff0e84d61a5d9ac27a046988289719c2
Elisa Situ Huang <elisasituhuang@gmail.com>
2022-10-14 23:17:56 -0400

    commit tree 777c8d4a4b4c561b810d0add409b7cedbc9ec4fd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity){
+    return `This Saturday, I want to ${activity}!`
+    }
+    else {
+        return 'This Saturday, I want to roller-skate!'
+    }
+ 
+    
+}
+function mondayWork(dosomething){
+  if (dosomething) {
+    return `This Monday, I will ${dosomething}.`
+  } else {
+    return "This Monday, I will go to the office."
+  }
+}
+function wrapAdjective (what) {
+    if (what){
+    return `You are ${what}!`
+    } else {
+        return '*'
+    }
+
+}
